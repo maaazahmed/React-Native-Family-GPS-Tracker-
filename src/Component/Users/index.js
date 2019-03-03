a9dfc2b3772398e75b00339944ce0ce017082147
Your Name <maazahmed2k16@gmail.com>
2019-03-03 14:00:11 -0800

    Updated

--- src/Component/Users/index.js
+++ src/Component/Users/index.js
@@ -20,20 +20,84 @@ class UserList extends Component {
     componentDidMount() {
         const currentUser = this.props.currentUser.currentUser;
         const circleList = this.props.navigation.state.params;
-        let arr = []
         database.child("Users").on("value", (snap) => {
+            let arr = []
             let users = snap.val()
             for (var key in users) {
                 arr.push({ ...users[key], key })
             }
-            this.props.UserListAction(arr)
+            // this.props.UserListAction(arr)
+            database.child(`Circle/${currentUser.uid}/${circleList.key}/AddedPeople/`).on("value", async (some) => {
+                let filteredData = []
+                var obj = some.val()
+                for (var id in obj) {
+                    filteredData.push({ ...obj[id], id })
+                }
+                console.log(arr, filteredData)
+                var array1 = arr;
+                var array2 = filteredData;
+
+                // var tempArr = array2.filter(function (item) {
+                //     console.log(item.key)
+                //     return !array1.includes(item.key);
+                // });
+                // array1 = array1.filter(function (item) {
+                //     return !array2.includes(item);
+                // });
+                // array2 = tempArr;
+
+                // console.log(array1); // [ 'a', 'c', 'e' ]
+                // console.log(array2); // [ 'f' ]
+                // var arr1 = [{
+                //     name: 'category1',
+                //     id: '1'
+                // }, {
+                //     name: 'category3',
+                //     id: '2'
+                // }, {
+                //     name: 'category2',
+                //     id: '2'
+                // }];
+
+                // var arr2 = [{
+                //     name: 'category2',
+                //     id: '5'
+                // }, {
+                //     name: 'category2',
+                //     id: '1'
+                // },
+                // {
+                //     name: 'category2',
+                //     id: '4'
+                // }, {
+                //     name: 'category2',
+                //     id: '2'
+                // }];
+
+
+                // let filtered = [];
+
+                // arr1.filter(function (newData) {
+                //     return arr2.filter(function (oldData) {
+                //         if (newData.id === oldData.id && newData.name === oldData.name) {
+                //             filtered.push({
+                //                 'id': newData.id,
+                //                 'name': newData.name
+                //             })
+                //         }
+                //     })
+                // });
+                // console.log(filtered)
+
+            })
         })
+
+
 
     }
 
     addUser(data) {
         const circleList = this.props.navigation.state.params;
-
         const currentUser = this.props.currentUser.currentUser;
         database.child(`Circle/${currentUser.uid}/${circleList.key}/AddedPeople/${data.key}`).set({
             name: data.username,
@@ -44,40 +108,34 @@ class UserList extends Component {
 
     render() {
         let userList = this.props.userList.userList
-        const circleList = this.props.navigation.state.params;
-        var myAdd = []
-        for (var key in circleList.AddedPeople) {
-            myAdd.push({ ...circleList.AddedPeople[key] })
-        }
         return (
             <View style={styles.container}>
                 <FlatList data={userList}
-                    renderItem={({ item }) => {
-                        return (
-                            <View style={styles.listContaineer}>
-                                <View style={styles.listImgeView} >
-                                    <Image
-                                        resizeMode="cover"
-                                        style={styles.Image}
-                                        source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpI5265EtpPkjBqJktStVkARbiCNap_IxsM2-aJC6GxuiKG96k" }} />
-                                </View>
-                                <View style={styles.nameEmail} >
-                                    <View style={styles.nameEmailView} >
-                                        <Text style={styles.nametext} >{item.username}</Text>
-                                        <Text style={styles.email} >{item.email}</Text>
-                                    </View>
-                                    <TouchableOpacity
-                                        onPress={this.addUser.bind(this, item)}
-                                        activeOpacity={0.5}
-                                        style={styles.IconContainer} >
-                                        <Icon name="person-add"
-                                            size={30}
-                                            color="#e91e8d" />
-                                    </TouchableOpacity >
-                                </View>
+                    renderItem={({ item }) => (
+                        <View style={styles.listContaineer}>
+                            <View style={styles.listImgeView} >
+                                <Image
+                                    resizeMode="cover"
+                                    style={styles.Image}
+                                    source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpI5265EtpPkjBqJktStVkARbiCNap_IxsM2-aJC6GxuiKG96k" }} />
                             </View>
-                        )
-                    }} keyExtractor={(item) => { return item.key }} />
+                            <View style={styles.nameEmail} >
+                                <View style={styles.nameEmailView} >
+                                    <Text style={styles.nametext} >{item.username}</Text>
+                                    <Text style={styles.email} >{item.email}</Text>
+                                </View>
+                                <TouchableOpacity
+                                    onPress={this.addUser.bind(this, item)}
+                                    activeOpacity={0.5}
+                                    style={styles.IconContainer} >
+                                    <Icon name="person-add"
+                                        size={30}
+                                        color="#e91e8d" />
+                                </TouchableOpacity >
+                            </View>
+
+                        </View>
+                    )} keyExtractor={(item) => { return item.key }} />
 
             </View>
         )
